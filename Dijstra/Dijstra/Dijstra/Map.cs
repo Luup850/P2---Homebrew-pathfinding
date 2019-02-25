03c1559806d381329f8533bfc6815cefaab01c90
Luup850 <marcusdchr@gmail.com>
2019-02-25 13:24:13 +0100

    s

---
+++ Dijstra/Dijstra/Dijstra/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijstra
+{
+    class Map
+    {
+        int XAmount;
+        int YAmount;
+        Tile[,] MapArray;
+
+        public Map(int xAmount, int yAmount)
+        {
+            XAmount = xAmount;
+            YAmount = yAmount;
+            MapArray = new Tile[xAmount, yAmount];
+        }
+    }
+
+    class Tile
+    {
+        bool Solid;
+        int X, Y;
+        int SetpsToGoal;
+    }
+
+    class Player
+    {
+
+    }
+}
