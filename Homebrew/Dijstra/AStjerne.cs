7aa26311e338df6d2d485d012e7dd56b147ca366
Luup850 <marcusdchr@gmail.com>
2019-03-08 13:34:11 +0100

    .

--- Homebrew/Dijstra/AStjerne.cs
+++ Homebrew/Dijstra/AStjerne.cs
@@ -13,31 +13,42 @@ namespace Dijstra
         int counter = 0;
         Tile NextTile;
 
+        /// <summary>
+        /// Finds a path to the goal
+        /// </summary>
+        /// <param name="map">Map that the player needs to search</param>
+        /// <param name="x">X start position</param>
+        /// <param name="y">Y start position</param>
         override public void FindPath(Map map, int x, int y)
         {
             int AwayFromstart = 0;
             map.MapArray[x, y].StepsFromStart = 0;
             map.MapArray[x, y].Visited = true;
+
+            //Continue while player's position is not equal to goal.
             while (map.GoalX != x || map.GoalY != y)
             {
-                map.GetConnections(map.MapArray[x, y]);
+                map.GetConnections(map.MapArray[x, y]); //Get connections from the node that the algorithm is on.
+                
+                //Run through all the nodes around the current node the algorithm is looking at.
                 for (int i = 0; i < map.MapArray[x, y].Connections.Count; i++)
                 {
+                    //Checks if the node has not been scanned.
                     if (map.MapArray[x, y].Connections.ElementAt(i).Visited != true)
                     {
 
-                        Open.Add(map.MapArray[x, y].Connections[i]);
+                        Open.Add(map.MapArray[x, y].Connections[i]);    //Add to list of potential nodes that can be visited.
                         
 
-                        map.MapArray[x, y].Connections.ElementAt(i).Visited = true;
+                        map.MapArray[x, y].Connections.ElementAt(i).Visited = true; //Node has been scanned
                         map.MapArray[x, y].Connections.ElementAt(i).StepsFromStart = AwayFromstart + 1;
                         map.MapArray[x, y].Connections.ElementAt(i).CalcTotalCost();
                     }
                 }
+
+                //Check which node is the best in terms of cost.
                 for(int i = 0; i < Open.Count; i++)
                 {
-
-                
                     if (Open.ElementAt(i).TotalCost < BestCost || counter == 0)
                     {
                         BestCost = Open.ElementAt(i).TotalCost;
@@ -59,13 +70,21 @@ namespace Dijstra
             }
             Route.Add(map.MapArray[x, y]);
             map.MapArray[x, y].Symbol = 'm';
+
+            //We've found the goal and has a list of all potential interesting nodes
+
+            //Start at goal and continue while distance from start is not 0.
             while (AwayFromstart != 0)
             {
                 map.GetConnections(map.MapArray[x, y]);
+
+                //Loop through all connections
                 for (int i = 0; i < map.MapArray[x, y].Connections.Count; i++)
                 {
+                    //If the node has been scanned, then its a possible route back.
                     if ( map.MapArray[x, y].Connections.ElementAt(i).Visited == true)
                     {
+                        //Check 
                         if (map.MapArray[x, y].Connections.ElementAt(i).StepsFromStart < AwayFromstart)
                         {
                             Route.Add(map.MapArray[x, y].Connections.ElementAt(i)); 
